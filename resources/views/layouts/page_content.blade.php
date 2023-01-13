5b4ccd69a3c2fe2459b312dacbbb3d7981695031
Samir ram <samirram007@gmail.com>
2023-01-13 14:41:21 +0530

    test update

--- resources/views/layouts/page_content.blade.php
+++ resources/views/layouts/page_content.blade.php
@@ -2,7 +2,7 @@
 
     <div class="row">
         <div class="col col-4 col-sm-4 col-md border border-3 border-danger">
-            {{-- <img src="{{asset('uploads/box1.jpg')}}" class="img-fluid" alt="" srcset=""> --}}
+            <img src="{{asset('uploads/box1.jpg')}}" class="img-fluid" alt="" srcset="">
         </div>
         <div class="col col-4 col-sm-4 col-md border border-3 border-danger"></div>
         <div class="col col-4 col-sm-4 col-md border border-3 border-danger"></div>
