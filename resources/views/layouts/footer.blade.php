1b5c7fce92304449710c0cb3d13a457d92d053a6
PriyanshuChourasia <105114609+PriyanshuChourasia@users.noreply.github.com>
2023-01-13 09:45:28 +0530

    13012023

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="fixed-bottom container-fluid bg-dark">
+    <div class="container web-footer">
+        <div>
+            <strong class="text-white">Copyright &copy; 2014-2021 <a href="">{{env('APP_NAME')}}</a> All rights
+                reserved.</strong>
+        </div>
+
+
+    </div>
+    <div class="mobile-footer">
+        <nav class="footer-nav">
+            <a href="" class="nav-link">
+
+                <span class="nav-text">Home</span>
+            </a>
+            <a href="" class="nav-link">
+                <span class="nav-text">About-Us</span>
+            </a>
+            <a href="" class="nav-link">
+                <span class="nav-text">Services</span>
+            </a>
+            <a href="" class="nav-link">
+                <span class="nav-text">Contacts</span>
+            </a>
+        </nav>
+    </div>
+</footer>
